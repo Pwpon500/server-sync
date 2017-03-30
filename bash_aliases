2858f5f377a0950606163918b19d91ffffcee3cb
root <root@train-master.lab.local>
2017-03-30 11:10:16 -0400

    Initial content commit

---
+++ bash_aliases
@@ -0,0 +1,11 @@
+alias lep='less info.txt | grep '\''p.*s.*w.*d\|secret'\'''alias lep='less info.txt | grep '\''p.*s.*w.*d\|secret'\'''
+alias svq='svn update; svn status'
+alias cvn='cd ~/svn_checkouts/; l'
+alias p='ping '
+alias rg="grep -i -r "
+alias fwip="rg '^ ip address' * | grep fw"
+alias swip="rg '^ ip address\|^  ip address' * | grep sw"
+alias pub="rg '^ ip address' * | grep gw- | grep -v ' 100\.[6][4-9]\| 100\.[7-9][0-9]\| 100\.[1][0-1][0-9] \| 100\.[12][0-7]\| 192\.168\| 10\.\| 172\.[1][6-9]\| 172\.[2][0-9]\| 172\.[3][0-1]'"
+alias pg="ping google.com"
+alias naut='natilus .'
+alias ssr='ssh -l root '
---
+++ initialize.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+from subprocess import call
+
+call("echo \"source ~/server-sync/vimrc\" > ~/.vimrc", shell=True)
+call("ln -s ~/server-sync/vim ~/.vim", shell=True)
+
+call("echo \". ~/server-sync/bash_aliases\" > ~/.bash_aliases", shell=True)
+if os.path.isfile("~/.bashrc"):
+    bashrc_content = open("~/.bashrc", "r").read()
+    if !bashrc_content.contains("/etc/bashrc"):
+        call("echo \"~/server-sync/bashrc\" > ~/.bashrc", shell=True)
+else:
+    call("echo \"~/server-sync/bashrc\" > ~/.bashrc", shell=True)
+
+call("git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim", shell=True)
+call("vim +PluginInstall +qall", shell=True)
